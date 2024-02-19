3c84c41ba872c6708b905f596060d4643d3fcf4f
Yaron Koren <yaron57@gmail.com>
2024-02-19 16:34:58 +0000

    Add handling for $wgDBprefix when creating pagepermissions table
    Change-Id: I850137b508b48ac7412f97128a40e0a958f73b99

--- db_patches/pagepermissions.sql
+++ db_patches/pagepermissions.sql
@@ -1,7 +1,7 @@
-CREATE TABLE pagepermissions (
+CREATE TABLE /*_*/pagepermissions (
 	pper_page_id int unsigned NOT NULL,
 	pper_user_id int unsigned NOT NULL,
 	pper_role varchar(60) NOT NULL,
 	pper_timestamp varbinary(14) NOT NULL default '',
 	PRIMARY KEY (pper_page_id, pper_user_id)
-)/*$wgDBTableOptions*/;
+) /*$wgDBTableOptions*/;
